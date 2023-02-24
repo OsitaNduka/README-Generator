2ec9e3b74057798a7e8906bcb198ee32c35369e2
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-02-24 22:05:07 +0000

    modified index.js

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = () => 
+   
+ inquirer.prompt([
+       {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the Title of Your Project?',
+       },
 
-];
+       {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a short description explaining the what, why, and how'
+       },
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
