2229200730d6c1c528a3586a68375a2b261c9613
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-02-27 17:14:55 +0000

    added function to generate markdown for README on generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,21 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
 `;
 }
 
