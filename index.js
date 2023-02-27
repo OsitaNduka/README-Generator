c6a7f43186294a9e34064ff1a112a848cb8a246b
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-02-27 16:25:08 +0000

    added prompt questions on index.js

--- index.js
+++ index.js
@@ -4,21 +4,56 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = () => 
-   
- inquirer.prompt([
-       {
-        type: 'input',
-        name: 'Title',
-        message: 'What is the Title of Your Project?',
-       },
-
-       {
-        type: 'input',
-        name: 'Description',
-        message: 'Provide a short description explaining the what, why, and how'
-       },
-]);
+const questions = () => {
+  return inquirer.prompt([
+      {
+           type: 'input',
+           name: 'github',
+           message: 'What is your GitHub username?',
+      },
+      { 
+           type: 'input',
+           name: 'email',
+           message: 'What is your email address?',
+      },
+      {
+           type: 'input',
+           name: 'Title',
+           message: 'What is the name/title of your project?',
+      },
+      {
+           type: 'input',
+           name: 'Description',
+           message: 'Please provide a short description explaining your project'
+      },
+      {
+           type: 'list',
+           message: 'What kind of license should your project have?',
+           name: 'license',
+           choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+          type: 'input',
+          name: 'Installation',
+          message: 'What command should be run to install dependencies?',
+      },
+      {
+          type: 'input',
+          name: 'Test',
+          message: 'What command should be run to run test?',
+      },
+      {
+          type: 'input',
+          name: 'Usage',
+          message: 'What does the user need to know about using the repo?',
+      },
+      {
+         type: 'input',
+         name: 'Contributing',
+         message: 'What does the user need to know about contributingto the repo?'
+      }
+  ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
